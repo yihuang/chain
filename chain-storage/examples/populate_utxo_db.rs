6459e08d7d01dab2edad3407168ac65c51c1d3c9
yihuang <yi.codeplayer@gmail.com>
2020-06-15 03:21:36 +0000

    temp

--- chain-storage/examples/populate_utxo_db.rs
+++ chain-storage/examples/populate_utxo_db.rs
@@ -2,6 +2,7 @@ use chain_storage::utxo::{flush_utxo_kvdb, UTxO, UTxOBuffer};
 use chain_storage::NUM_COLUMNS;
 use rand::Rng;
 use std::env;
+use std::time::SystemTime;
 
 fn random_utxo() -> UTxO {
     UTxO {
@@ -10,7 +11,9 @@ fn random_utxo() -> UTxO {
     }
 }
 
-fn populate(path: &str, count: usize) {
+fn populate(path: &str, count: usize, version: u64) {
+    println!("begin: {:?}", std::time::SystemTime::now());
+
     let store = kvdb_rocksdb::Database::open(
         &kvdb_rocksdb::DatabaseConfig::with_columns(NUM_COLUMNS),
         path,
@@ -18,13 +21,16 @@ fn populate(path: &str, count: usize) {
     .unwrap();
     let utxos = (0..count).map(|_| random_utxo());
     let buffer = utxos.map(|utxo| (utxo, Some(()))).collect::<UTxOBuffer>();
-    flush_utxo_kvdb(&store, buffer, 0).unwrap();
+    println!("prepare data: {:?}", std::time::SystemTime::now());
+    flush_utxo_kvdb(&store, buffer, version).unwrap();
+    println!("after flush: {:?}", std::time::SystemTime::now());
 }
 
 fn main() {
     let mut args = env::args().skip(1);
     let path = args.next().unwrap();
     let count = args.next().unwrap().parse::<usize>().unwrap();
-    println!("{} {}", path, count);
-    populate(&path, count);
+    let version = args.next().unwrap().parse::<u64>().unwrap();
+    println!("{} {} {}", path, count, version);
+    populate(&path, count, version);
 }
